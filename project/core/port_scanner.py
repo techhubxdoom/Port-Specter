5fb97d3ae377b37a869e737a1c93aef29294e249
techhubxdoom <xtechxhubx@gmail.com>
2026-02-18 11:24:40 +0300

    editing port scanner and adding is admin and check if in system32

--- project/core/port_scanner.py
+++ project/core/port_scanner.py
@@ -1,6 +1,9 @@
 import psutil
 import ipaddress
 from logging_system import SystemLogger
+import ctypes
+import ctypes.wintypes as wintypes
+import os
 
 
 class PortScanner:
@@ -12,19 +15,17 @@ class PortScanner:
     #--------------- get opened connections ---------------#
     def get_connections(self, conn_type:str):
         connections = []
+        conn_type = conn_type.upper()
 
-        #: set the connection type who mmost be scanned :#
-        if conn_type.lower() == 'listen':
-            status_type = psutil.CONN_LISTEN
-        elif conn_type.lower() == 'established':
-            status_type = psutil.CONN_ESTABLISHED
-        else:
-            return 'conn_type most be - listen or established -'
-        
+    
+        #: check conn_type :#
+        if conn_type not in ['ESTABLISHED', 'LISTEN']:
+            return f'{conn_type} not valid!\nconnect type most be - ESTABLISHED OR LISTEN -'
+            
 
         for conn in psutil.net_connections(kind="inet"):
 
-            if conn.status != status_type:
+            if conn.status != conn_type:
                 continue
             
             #- in listen connection -#
@@ -35,7 +36,7 @@ class PortScanner:
             }
 
             #- just when connection established -#
-            if status_type == psutil.CONN_ESTABLISHED:
+            if conn_type == psutil.CONN_ESTABLISHED:
                 remote_ip = conn.raddr.ip if conn.raddr else None
                 remote_port = conn.raddr.port if conn.raddr else None
 
@@ -52,8 +53,18 @@ class PortScanner:
                         "status": proc.status(),
                         "exe": proc.exe(),
                     }
+
+                    #- check if runing in system32 -#
+                    exe_path = proc.exe()
+                    connection_data["is_system32"] = self.is_in_system32(exe_path)
+
+
+                    #- check if runing as admin -#
+                    connection_data['is_admin'] = self.is_runing_as_admin(conn.pid)
+
                 except psutil.AccessDenied:
                     connection_data["process"] = "Access Denied"
+                    connection_data["is_admin"] = False
 
             #- adding risk score
             connection_data["risk_score"] = self.calculate_risk(connection_data)
@@ -139,7 +150,83 @@ class PortScanner:
         if local_ip == "0.0.0.0":
             score += 30
 
+        if conn.get("is_admin"):
+            score += 20
+                                
+        if not conn.get("is_system32") and conn.get("is_admin"):
+            score += 30
+
         return min(score, 100)
+    
+    
+    #--------------- check if program runing as admin ---------------#
+    def is_runing_as_admin(self, pid):
+        try:
+            PROCESS_QUERY_LIMITED_INFORMATION = 0x1000
+            TOKEN_QUERY = 0x0008
+            TokenElevation = 20
+
+            kernel32 = ctypes.windll.kernel32
+            advapi32 = ctypes.windll.advapi32
+
+            hProcess = kernel32.OpenProcess(
+                PROCESS_QUERY_LIMITED_INFORMATION,
+                False,
+                pid
+            )
+
+            if not hProcess:
+                return False
+
+            hToken = wintypes.HANDLE()
+
+            if not advapi32.OpenProcessToken(
+                hProcess,
+                TOKEN_QUERY,
+                ctypes.byref(hToken)
+            ):
+                kernel32.CloseHandle(hProcess)
+                return False
+
+            elevation = wintypes.DWORD()
+            size = wintypes.DWORD()
+
+            result = advapi32.GetTokenInformation(
+                hToken,
+                TokenElevation,
+                ctypes.byref(elevation),
+                ctypes.sizeof(elevation),
+                ctypes.byref(size),
+            )
+
+            #- close handles properly
+            kernel32.CloseHandle(hToken)
+            kernel32.CloseHandle(hProcess)
+
+            if not result:
+                return False
+
+            return bool(elevation.value)
+
+        except:
+            return False
+        
+    
+    #--------------- check if program runing in System32 ---------------#
+    def is_in_system32(self, exe_path):
+        if not exe_path:
+            return False
+
+        exe_path = exe_path.lower()
+
+        system32_path = os.path.join(
+            os.environ.get("WINDIR", "C:\\Windows"),
+            "System32"
+        ).lower()
+
+        return True if exe_path.startswith(system32_path) else False
+
+
 
 
 x = PortScanner('listen')
